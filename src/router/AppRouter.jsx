784abaecf4edcbb5f7a5d6e12679cff54c9bdc30
felix <felixfranko66@gmail.com>
2024-04-26 20:43:04 +0300

    p

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
