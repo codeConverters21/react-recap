00c07cea58736dfca801d1679a57903ef05fd4c2
felix <felixfranko66@gmail.com>
2024-04-26 21:39:07 +0300

    p

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
 
 const Products = () => {
-  return <div>Products</div>;
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => setProducts(res.data))
+      .catch((err) => console.log(err));
+  };
+
+  // const getProducts = async () => {
+  //   try {
+  //     const { data } = await axios.get("https://fakestoreapi.com/products");
+  //     console.log(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  return (
+    <div>
+      <h1>Products</h1>
+      {products.map((product) => (
+        <ProductCard />
+      ))}
+    </div>
+  );
 };
 
 export default Products;
