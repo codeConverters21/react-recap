67aa423dba429018a3c77a0ae63cc82b765f189d
felix <felixfranko66@gmail.com>
2024-04-26 20:45:30 +0300

    p

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Products from "../pages/Products";
+import Contact from "../pages/Contact";
+import Notfound from "../pages/Notfound";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Notfound />} />
       </Routes>
     </BrowserRouter>
   );
